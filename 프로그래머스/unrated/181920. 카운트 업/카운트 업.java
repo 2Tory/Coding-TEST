bc5dc896f4e809f12e6ec66f54411d46dbdf335e
2Tory <50188172+2Tory@users.noreply.github.com>
2023-12-06 21:43:42 +0900

    [level unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i = start_num; i<=end_num; i++){
+            answer.add(i);
+        }
+        
+        
+        return answer;
+    }
+}
